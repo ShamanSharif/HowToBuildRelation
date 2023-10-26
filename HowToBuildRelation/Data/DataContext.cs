8e35992e87688a17e7156b2288bebe0faddd9715
shaman <shamansharif@inneed.cloud>
2023-10-26 12:26:34 +0600

    Init

---
+++ HowToBuildRelation/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HowToBuildRelation.Data;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) 
+    { }
+}
